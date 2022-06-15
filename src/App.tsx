4b62bd9af65377d51c2c5e39daa7cfc9be60c307
MerciMarcy <masashi.krt74000011@gmail.com>
2022-06-15 21:45:21 +0900

    chart_lesson完了

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import BarPlot from "./components/BarPlot";
+import BubblePlot from "./components/BubblePlot";
+import LinePlot from "./components/LinePlot";
+import MaterialUI from "./components/MaterialUI";
+import PiePlot from "./components/PiePlot";
+import RadarPlot from "./components/RadarPlot";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LinePlot />
+      <PiePlot />
+      <BarPlot />
+      <RadarPlot />
+      <BubblePlot /> */}
+
+      <MaterialUI />
+    </div>
+  );
+}
+
+export default App;
